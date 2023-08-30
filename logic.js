6faea3b443258712ca0a5cbfe3eaf63268b29b93
SkepticalYouth <chamber2002@gmail.com>
2023-08-30 14:10:19 +0700

    Fleshing out dialog logic

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(author,title,pages,readStatus) {
     this.author = author
@@ -11,17 +11,6 @@ function addBooktoLibrary(book) {
     myLibrary.push(book)
 }
 
-const newBook1 = new Book('William Rehnquist', 'The Supreme Court',336,'Read')
-const newBook2 = new Book('Charles Dickens', 'David Copperfield',1083,'Read')
-myLibrary.push(newBook1, newBook2)
-
-myLibrary.forEach(book => {
-    const bookElement = document.createElement('div');
-    bookElement.innerHTML = `Title: ${book.title}<br> Author: ${book.author}<br> Pages: ${book.pages}<br> Status: ${book.readStatus}`;
-    bookElement.classList.add('book')
-    document.getElementsByClassName('card-container')[0].appendChild(bookElement);
-});
-
 document.querySelector('.new').addEventListener('click', ()=>{
     document.getElementById('add-dialog').showModal()
 })
@@ -29,3 +18,56 @@ document.querySelector('.new').addEventListener('click', ()=>{
 document.getElementById('cancel').addEventListener('click', ()=>{
     document.getElementById('add-dialog').close()
 }) 
+
+function displayBook(bookInfo){
+    const bookElement = document.createElement('div');
+    const buttonDiv = document.createElement('div')
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove'
+    bookElement.innerHTML = `Title: ${bookInfo.title}<br> Author: ${bookInfo.author}<br> Pages: ${bookInfo.pages}<br> Status: ${bookInfo.readStatus}`;
+    buttonDiv.appendChild(removeButton)
+    buttonDiv.classList.add('buttonDiv')
+    bookElement.appendChild(buttonDiv)
+    bookElement.classList.add('book')
+    removeButton.classList.add('remove')
+    document.querySelector('.card-container').appendChild(bookElement);
+}
+
+function resetDialog(){
+    document.getElementById('author').value = ''
+    document.getElementById('title').value = ''
+    document.getElementById('pages').value = ''
+    readStatus = document.getElementById('status')
+    readStatus.value= ""
+}
+
+function confirmLibraryBook(Event){
+    let bookInfo = []
+    author = document.getElementById("author")
+    title = document.getElementById("title")
+    pages = document.getElementById("pages")
+    readStatus = document.getElementById('status')
+    readStatusInsert = readStatus.options[readStatus.selectedIndex]
+
+    bookInfo['author'] = author.value
+    bookInfo['title'] = title.value
+    bookInfo['pages'] = pages.value
+    bookInfo['readStatusInsert'] = readStatusInsert.text
+    let newBook = new Book(bookInfo.author, bookInfo.title, bookInfo.pages, bookInfo.readStatusInsert)
+    addBooktoLibrary(newBook)
+    Event.preventDefault()
+    document.getElementById('add-dialog').close()
+    resetDialog()
+    displayBook(newBook)
+}
+
+function processInfo(){
+    document.getElementById("confirmBtn").addEventListener('click', confirmLibraryBook)
+    document.getElementById('confirmBtn').addEventListener('click', confirmLibraryBook)
+}
+
+processInfo()
+
+
+
+
